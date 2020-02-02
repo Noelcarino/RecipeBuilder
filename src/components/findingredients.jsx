099b896d4ee0d5a420c18035eaaca4ee6d772fbd
Noel Carino <leononirac1992@gmail.com>
2020-02-02 01:14:26 -0800

    2/1 | Set up background for 'find-ingredients-component

--- src/components/findingredients.jsx
+++ src/components/findingredients.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../css/findingredients.css'
 
 export default class FindIngredients extends React.Component {
     render(){
         return (
-            <div>
+            <div className="find-ingredients-component-container">
                 <button className="btn btn-dark text-muted" onClick={() => this.props.setView('dashboard')}>
                     <h1>
                         PAGE UNDER CONSTRUCTION | BACK TO DASHBOARD
