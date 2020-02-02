2d2dd5ff405af568506f5b25b8ee2d8af5d9ab5a
Noel Carino <leononirac1992@gmail.com>
2020-02-01 16:51:03 -0800

    2/1 | Cleaned up files and started initialized RecipeBuilder Component

---
+++ src/components/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+    render(){
+        return (
+            <div>
+                THIS IS THE HOME PAGE
+            </div>
+        )
+    }
+}
