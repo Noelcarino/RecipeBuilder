5ea9c36afcf40a64d53cbf851f8d00f3fe451ee7
Noel Carino <leononirac1992@gmail.com>
2020-02-03 17:34:44 -0800

    2/3 | Set up feature-4 description section

--- src/components/confirmingredients.jsx
+++ src/components/confirmingredients.jsx
@@ -3,10 +3,22 @@ import '../css/confirmingredients.css';
 export default class ConfirmIngredients extends React.Component {
     render(){
         return (
-            <div className="confirm-ingredients-container">
-                <button onClick={() => this.props.setView('findingredients')}>
-                    RETURN TO SEARCH
-                </button>
+            <div className="confirm-ingredients-container row px-0 py-5 mx-auto">
+
+                <div className="confirm-ingredients-description mx-auto mb-5 text-center p-3">
+                    <h1>Ingredients List</h1>
+
+                    <h3>
+                        Confirm your ingredients and build your recipe!
+                    </h3>
+                </div>
+
+                <div className="confirm-ingredients-content container-fluid mx-auto">
+                    <button onClick={() => this.props.setView('findingredients')}>
+                        RETURN TO SEARCH
+                    </button>
+                </div>
+
             </div>
         )
     }
