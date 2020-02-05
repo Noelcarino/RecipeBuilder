5b69cea91efd620fc886906022380740c31525e0
Noel Carino <leononirac1992@gmail.com>
2020-02-05 12:16:27 -0800

    2/5 | Sent data required to use to component 'letscook'

--- src/components/recipebuilder.jsx
+++ src/components/recipebuilder.jsx
@@ -5,30 +5,42 @@ import DashBoard from './dashboard';
 import FindIngredients from './findingredients';
 import ConfirmIngredients from './confirmingredients';
 import RecommendedRecipes from './recommendedrecipes';
+import LetsCook from './letscook';
 
 export default class RecipeBuilder extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             view : {
-                name: 'homepage',
+                name: 'recommendedrecipes',
                 params: {
                     ingredientsToUse: [],
+                    currentRecipeToCook: {}
                 }
             }
         }
         this.setView = this.setView.bind(this);
     }
     setView(name, param){
+        console.log('name - ', name);
+        console.log('param - ', param);
+        let currentRecipe;
         if (param === undefined) param = [];
+        else if (param.recipeId) {
+            currentRecipe = param;
+            param = currentRecipe.recipeIngredients;
+        }
         this.setState({
             view: {
                 name:name,
                 params: {
-                    ingredientsToUse: param
+                    ingredientsToUse: param,
+                    currentRecipeToCook: currentRecipe
                 }
             }
         });
+        console.log(this.state);
+        console.log(param.recipeId);
     }
     render(){
         const { name } = this.state.view;
@@ -59,6 +71,12 @@ export default class RecipeBuilder extends React.Component {
                 element = <RecommendedRecipes
                                 setView={this.setView}
                                 confirmedIngredients={this.state.view.params.ingredientsToUse}
+                            />
+                break;
+            case 'letscook':
+                element = <LetsCook
+                                setView={this.setView}
+                                state={this.state}
                             />
                 break;
             default:
