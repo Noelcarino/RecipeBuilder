640146ee46cee730815b46784aea0eba250c6c10
Noel Carino <leononirac1992@gmail.com>
2020-02-04 17:49:07 -0800

    2/4 | Set up recipe component information, moving on to adding pitures for recipes

--- src/components/confirmedrecipe.jsx
+++ src/components/confirmedrecipe.jsx
@@ -3,8 +3,22 @@ import React from 'react';
 export default class ConfirmedRecipe extends React.Component{
     render(){
         return (
-            <div>
-                {this.props.recipeTitle}
+            <div className="confirmed-recipe mx-auto mb-5 row align-items-end">
+                
+
+                <div className="recipe-bottom-container mx-auto row align-items-end border py-2">
+
+                    <div className="container-fluid mx-auto text-center p-0">
+                        {this.props.recipeTitle}
+                    </div>
+
+                    <div className="container-fluid mx-auto  my-0 py-0 row">
+                        <button className="btn btn-dark mx-auto" onClick={() => console.log(this.props)}>
+                            LET'S COOK!
+                        </button>
+                    </div>
+
+                </div>
             </div>
         )
     }
